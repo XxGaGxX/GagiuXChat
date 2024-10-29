f3cdc81c5d21fe29025326366e371b34eca800cf
diego vagnini <diego.vagnini6@gmail.com>
2024-10-29 08:40:49 +0100

    progetto finito => solo da testare

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
@@ -54,9 +56,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Chat(messages: messages, //abbiniamo il widget con i messaggi 
-      onSendPressed: onSendPressed, 
-      user: user),
+        body: Chat(
+      messages: messages, //abbiniamo il widget con i messaggi
+      onPreviewDataFetched: _handlePreviewDatafetched,
+      onSendPressed: _handleSendPressed,
+      showUserAvatars: true,
+      showUserNames: true,
+      user: _user,
+      theme: const DefaultChatTheme(
+        seenIcon: Text(
+          "read",
+          style: TextStyle(fontSize: 10),
+        ),
+      ),
     ));
   }
 
@@ -67,9 +79,32 @@ class _MyHomePageState extends State<MyHomePage> {
             as List) // Lo deserializziamo in List di string
         .map((e) => types.Message.fromJson(e as Map<String,
             dynamic>)) //mappiamo  ogni elemento della lista in un oggetto Message
-        .toList(); //Lo convertiamo in una lista di oggetti
+        .toList(); //Lo cogit add .nvertiamo in una lista di oggetti
     setState(() {
       messages = _messages;
+    });
+  }
+
+  void _handlePreviewDatafetched(types.TextMessage p1, types.PreviewData p2) {
+    final int index =
+        messages.indexWhere((types.Message element) => element.id == p1.id);
+    final types.Message updatedMessage =
+        (messages[index] as types.TextMessage).copyWith(previewData: p2);
+    /*Questa funzione recupera i messaggi contenuti nei messaggi.json e li mette sul body della mia
+    applicazione. In particola trova l’indice del messaggio nella lista _messaggi, ne crea una copia ed
+    aggiorna la lista*/
+  }
+
+  void _handleSendPressed(types.PartialText p1) {
+    final types.TextMessage textMessage =
+        types.TextMessage(author: _user, id: const Uuid().v4(), text: p1.text);
+
+    addMessage(textMessage);
+  }
+
+  void addMessage(types.TextMessage message) {
+    setState(() {
+      messages.insert(0, message);
     });
   }
 }
